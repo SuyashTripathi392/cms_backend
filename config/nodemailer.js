5173bc8ec99f7037f851d848c94f12c68dc3b73f
Suyash Tripathi <rajtr3142@gmail.com>
2025-12-19 22:37:08 +0530

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,34 +1,34 @@
-// import nodemailer from 'nodemailer';
-
-// const transporter = nodemailer.createTransport({
-//     host: "smtp-relay.brevo.com",
-//     port: 587,
-//     auth: {
-//         user: process.env.SMTP_USER,
-//         pass: process.env.SMTP_PASS
-//     }
-// });
-
-// export default transporter;
-
-
-
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  pool: true,
-  maxConnections: 1,
-  maxMessages: 100,
-  tls: {
-    rejectUnauthorized: false,
-  },
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
 });
 
 export default transporter;
+
+
+
+// import nodemailer from "nodemailer";
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp-relay.brevo.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+//   pool: true,
+//   maxConnections: 1,
+//   maxMessages: 100,
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// export default transporter;
